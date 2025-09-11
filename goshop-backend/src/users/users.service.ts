a4f00da99a6fa82439526550038065daaf790732
Bhairavi <Bhairavi.Gawas@flrs.co.uk>
2025-09-11 13:20:34 +0530

    feat: got the data to be stored in db, hashed password

--- goshop-backend/src/users/users.service.ts
+++ goshop-backend/src/users/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserRequestDto } from './dto/create-user.request.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequestDto) {
-    console.log('Creating user with data:', data);
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(data: CreateUserRequestDto): Promise<User> {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await bcrypt.hash(data.password, 10),
+      },
+    });
   }
 }
