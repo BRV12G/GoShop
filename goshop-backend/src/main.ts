f0e791950867870fc57a1891efd8c90cefb61de4
Bhairavi <Bhairavi.Gawas@flrs.co.uk>
2025-09-11 10:53:16 +0530

    feat: connecting the env to the system

--- goshop-backend/src/main.ts
+++ goshop-backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
