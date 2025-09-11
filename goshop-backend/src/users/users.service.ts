1b4e9dfe205630a4126197666d1adda7fb3c3876
Bhairavi <Bhairavi.Gawas@flrs.co.uk>
2025-09-11 13:34:53 +0530

    feat: added a error condition  for same user if created

--- goshop-backend/src/users/users.service.ts
+++ goshop-backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequestDto } from './dto/create-user.request.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
@@ -8,11 +8,19 @@ import * as bcrypt from 'bcrypt';
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
   async createUser(data: CreateUserRequestDto): Promise<User> {
-    return this.prismaService.user.create({
-      data: {
-        ...data,
-        password: await bcrypt.hash(data.password, 10),
-      },
-    });
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+      });
+    } catch (error) {
+         //   console.error('Error creating user:', error);
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw error;
+    }
   }
 }
