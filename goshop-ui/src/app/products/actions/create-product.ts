b473b9fa48c33b51f5402d871d4927c24e5fcc36
Bhairavi <bhairavigawas9@gmail.com>
2025-09-20 15:20:03 +0530

    feat: adding a new product and it fetches on the screen in realtime without refresh

--- goshop-ui/src/app/products/actions/create-product.ts
+++ goshop-ui/src/app/products/actions/create-product.ts
@@ -1,8 +1,11 @@
 // server action for products
 "use server";
 import { post } from "@/app/common/util/fetch";
+import { revalidateTag } from "next/cache";
 export default async function createProduct(
     formData: FormData
 ) {
-    return post("products", formData)
+    const response = await post("products", formData)
+    revalidateTag("products")
+    return response;
  }
