44a343ed4d54a42b9dd1f69c7f14957e67f45d77
Bhairavi <bhairavigawas9@gmail.com>
2025-09-20 17:30:52 +0530

    refactor: deleting unnecessary imports

--- goshop-ui/src/app/layout.tsx
+++ goshop-ui/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { Container, CssBaseline, ThemeProvider } from "@mui/material";
-import darkTheme from "./dark.theme";
+import { Container, CssBaseline } from "@mui/material";
 import Header from "./header/header";
 import Providers from "./providers";
 import authenticated from "./auth/authenticated";
